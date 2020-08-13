c2048651e85ea7d28b95e56261b28b7c08b532e8
alxndrdiaz <ra.ramos.diaz@gmail.com>
2020-08-13 15:30:12 +0200

    Updated comments
    Removed redundant comments and lines.

--- attractor_analysis/attractor_analysis.py
+++ attractor_analysis/attractor_analysis.py
@@ -1,14 +1,9 @@
-#~ language: Python 2.7.6
-#~ libraries:
-#~ numpy 1.8.2
-#~ pandas 0.17.1
-#~ scipy 0.13.3
+# Python 2.7.6
+# libraries: numpy 1.8.2; pandas 0.17.1; scipy 0.13.3
 
-#~ Attractor state analysis: this code takes the output attractors from hclust_mix
-#~ in .ats format and analyze overrepresented and underrepresented genes, which
-#~ are the predictions of the identified attractors. It also compares attractors
-#~ in order to determine if they are equal or if they are different and how they
-#~ are structured. Finally it provides output files for further analysis.
+# takes the output attractors from hclust_mix in .ats format and analyze overrepresented and  underrepresented genes, which are the predictions of the 
+# identified attractors. It also compares attractors in order to determine if they are different and how they are structured, provides output files 
+# for further analysis (.txt files, inlcuding files containing genes per binary state: -1, +1 ; and a summary file).
 
 import os
 import glob
@@ -16,52 +11,47 @@ import time
 import numpy as np
 import pandas as pd
 
-#~~>step0: reading files
 
-#~ detects current working directory and assigns it to the currentdirectory string
+# detects current working directory and assigns it to the currentdirectory string
 currentdirectory = os.getcwd()
 
-#~ reads all ats intermediate files in currentdirectory and assigns them to the list alltmp
+# reads all ats intermediate files in currentdirectory and assigns them to a list
 all_ats = glob.glob(currentdirectory + "/*.ats")
 
-#~ number of attractors to analyze
+# number of attractors to analyze
 filenumber = len(all_ats)
 
-#~ generates empty lists
-attlist = [] #~ storages all attractors in table format
-attpositions = [] #~ storages stage positions of all attractos
-undernumbers = [] #~ storages number of overrepresented genes in each attractor
-overnumbers = [] #~ storages number of underrepresented genes in each attractor
+# results lists
+attlist = [] # storages all attractors in table format
+attpositions = [] # storages stage positions of all attractos
+undernumbers = [] # storages number of -1 genes in each attractor
+overnumbers = [] # storages number of +1 genes in each attractor
 
-#~~> step1: basic analysis of each attractor
 
-#~ generates a list of attractor files by filling the empty list, then
-#~ classifies genes according to overrepresentation/underrepresented (+1 or -1)
+# classifies genes according to their binary state (-1 or +1) in each attractor  
 for at in all_ats:
     attdata = pd.read_table(at, index_col=0, header=0)
     attlist.append(attdata)
-    attstage = attdata.columns[0] #~ obtains index name for further selection
+    attstage = attdata.columns[0] 
     attpositions.append(attstage)
-    #~ underrepresentation selection analysis:
-    underset = attdata[attdata[attstage] == -1] #~ selects underrepresented genes
-    under_gene_list =  underset.index.tolist() #~ generates a list only with gene names
+    # search genes with binary state -1
+    underset = attdata[attdata[attstage] == -1] 
+    under_gene_list =  underset.index.tolist() 
     unumber = len(under_gene_list)
     undernumbers.append(unumber)
-    undersetname =  attstage + "_attractor_" + "under.txt"
-    #~ overrepresentation selection analysis:
-    overset = attdata[attdata[attstage] == 1]  #~ selects overrepresented genes
+    undersetname =  attstage + "_attractor_" + "down.txt"
+    # search genes with binary state +1
+    overset = attdata[attdata[attstage] == 1]  
     over_gene_list = overset.index.tolist()
     onumber = len(over_gene_list)
     overnumbers.append(onumber)
-    oversetname =  attstage + "_attractor_" + "over.txt"
+    oversetname =  attstage + "_attractor_" + "up.txt"
 
-#~ using the attractor list to create a file containing all attractors
+# using the attractor list to create a file containing all attractors
 all_attractors = pd.concat(attlist, axis=1)
 all_attractors.to_csv("all_atrractors.aat", sep="\t")
 
-#~~> identifies repeated and complementary attractors and provides a list of the results
-#~ it also provides an statistical summary and graphics.
-
+# identifies repeated and complementary attractors and provides a list of the results
 vectors = []
 repeated_attractors = []
 
@@ -82,14 +72,12 @@ for i in iterate:
               repeated = "(" + aux_positions[i] + "," + aux_positions[j] + ")"
               pairs_repeated.append(repeated)
 
-
 itpairs = np.arange(len(pairs_repeated))
 updated_pairs = []
-for i in itpairs: #~ prints only the pair of attractors by selecting only odd positions
+for i in itpairs: # prints only the pair of attractors by selecting only odd positions
     j = i + 1
     if j % 2 != 0:
        updated_pairs.append(pairs_repeated[j])
-
 
 genenumbers = np.vstack((undernumbers, overnumbers))
 genenumbers = np.transpose(genenumbers)
