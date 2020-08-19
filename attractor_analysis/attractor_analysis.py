298f0e80b69c008656acc0789490b9544a3ed1c3
alxndrdiaz <ra.ramos.diaz@gmail.com>
2020-08-19 21:25:26 +0200

    Update attractor_analysis.py

--- attractor_analysis/attractor_analysis.py
+++ attractor_analysis/attractor_analysis.py
@@ -11,35 +11,36 @@ import time
 import numpy as np
 import pandas as pd
 
-# detects current working directory and assigns it to the currentdirectory string
+# detects current working directory and assigns it to the currentdirectory string:
 currentdirectory = os.getcwd()
 
-# reads attractor file
+# reads attractor file:
 att_file = ''.join( glob.glob(currentdirectory + "/*.ats") )
 attractors = pd.read_csv(att_file, index_col=0, header=0, sep="\t")
 print
 print 'genes, samples = ', attractors.shape
 print
 
-# finds unique attractors
+# finds unique attractors:
 U_attractors = attractors.T
 U_attractors = U_attractors.drop_duplicates(keep='first')
 U_attractors = U_attractors.T
 print 'total unique attractors = ', U_attractors.shape[1]
 print
 
-# iterate to extract genes by state (-1,+1) for each attactor
+# iterate to extract genes by state (-1,+1) for each attactor:
 for column in U_attractors:
     low_states = U_attractors[column][ U_attractors[column] == -1 ]
     high_states = U_attractors[column][ U_attractors[column] == +1 ]
-    outname = str(column) + '_sample_attractor'
+    outname = 'A'+ str(column) + '_state'
     U_attractors[column].to_csv( outname + '.tab', index=True, header=True, sep="\t")  
-    low_states.to_csv( outname + '_low.ids', index=True, header=False, sep="\t")
-    high_states.to_csv(  outname + '_high.ids', index=True, header=False, sep="\t") 
+    low_states.to_csv( outname + '_genes_low.ids', index=True, header=False, sep="\t")
+    high_states.to_csv(  outname + '_genes_high.ids', index=True, header=False, sep="\t") 
+
     
-# find samples that converged to the same attractor
+# find samples that converged to the same attractor:
 atts_samples = []
-samples_sorted = [None]*len(attractors.columns)
+atts_sorted = [None]*len(attractors.columns)
 
 for column in U_attractors:
     att_by_sample = []
@@ -47,7 +48,7 @@ for column in U_attractors:
         msample = int(nsample) - 1
         if U_attractors[column].equals( attractors[nsample] ) == True:
            att_by_sample.append( nsample )
-           samples_sorted[msample] = 'A' + column
+           atts_sorted[msample] = 'A' + column
     atts_samples.append(att_by_sample)
 
 print
@@ -55,11 +56,13 @@ for atsample in atts_samples:
     print 'total samples converged to attractor = ', len(atsample); print 
 print
 
+samples_to_attractors = pd.DataFrame( 
+{'sample': list(attractors.columns),
+'attractor': atts_sorted} )
 
-print
-for sample in samples_sorted: 
-    print sample
-print
+samples_to_attractors = samples_to_attractors.reindex( columns = ['sample','attractor'] )
+samples_to_attractors.to_csv('samples_attractors.tab', index=False, header=True, sep="\t")
+
 
 
 
